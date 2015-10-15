ede700cca44aaa9420ea0ac6c00218bd0a54ebef
Charles Harrington <charles.harrington@gmail.com>
2015-10-14 21:38:19 -0700

    Add tiefighter class. Now giant tiefighters appear on screen.

---
+++ starWars/src/tiefighter.js
@@ -0,0 +1,12 @@
+var Tiefighter = function(x, y, z, timeBetweenSteps, docHeight, docWidth) {
+  MoveStar.apply(this, arguments);
+  this.$node = $('<img class="tiefighter" src="img/tiefighter.png"></img>');
+};
+
+Tiefighter.prototype = Object.create(MoveStar.prototype);
+Tiefighter.prototype.constructor = Tiefighter;
+
+// Tiefighter.prototype.step = function(){
+//   MoveStar.prototype.step.call(this);
+// };
+
